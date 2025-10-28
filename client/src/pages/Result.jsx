63572e5e48de879c309b0e3c04c710502330da97
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2025-10-29 00:58:14 +0530

    Buy Credit

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -4,8 +4,13 @@ import { assets } from '../assets/assets'
 const Result = () => {
   const [images , setImage ] = useState(assets.sample_img_1)
   const [isImageLoaded , setIsImageLoaded] = useState(false)
+  const [loading , SetLoading] = useState(false)
+  const [input  , setInput] = useState('')
+  const onSubmitHandler = async (e)=>{
+     e.preventDefault(); 
+  }
   return (
-    <form className="flex flex-col min-h-[90vh] justify-center items-center px-4">
+    <form  onSubmit={onSubmitHandler} className="flex flex-col min-h-[90vh] justify-center items-center px-4">
       <div className="flex flex-col items-center">
         <div className="relative">
           <img
@@ -13,12 +18,12 @@ const Result = () => {
             alt="Generated"
             className="max-w-sm w-full rounded-2xl shadow-lg hover:scale-105 transition-transform duration-500"
           />
-          <span className="absolute bottom-0 left-0 h-1 bg-blue-500 w-full animate-[loadingBar_10s_linear_infinite]" />
+          <span className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${loading ? 'w-full transition-all duration-[10s]':'w-0'}`} />
         </div>
-        <p className="mt-4 text-gray-600 text-sm tracking-wide animate-pulse">Generating your image...</p>
+        <p className={`mt-4 text-gray-600 text-sm tracking-wide animate-pulse ${!loading ? 'hidden ': ''}`}>Generating your image...</p>
       </div>
     {!isImageLoaded &&  <div className="flex w-full max-w-2xl bg-white/80 backdrop-blur-md shadow-md border border-gray-200 text-gray-800 text-sm p-1 mt-7 rounded-full overflow-hidden">
-        <input
+        <input onChange={e => setInput(e.target.value)} value={input}
           type="text"
           placeholder="Describe what you want to generate..."
           className="flex-1 bg-transparent outline-none px-6 py-3 text-gray-700 placeholder-gray-400"
@@ -31,7 +36,7 @@ const Result = () => {
         </button>
       </div> }
       {isImageLoaded && <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-7 rounded-full'>
-        <p className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>Generate Another</p>
+        <p onClick={()=>{setIsImageLoaded(false)}} className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>Generate Another</p>
         <a href={images}  download className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer ' >Download</a>
       </div>}
     </form>
